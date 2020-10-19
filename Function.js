bfc1f6ac416cd2cca8cbd91460f64606b18d568d
yuuki1218 <58942798+yuuki1218@users.noreply.github.com>
2020-10-19 10:58:03 +0900

    change

--- Function.js
+++ Function.js
@@ -1,87 +1,84 @@
-class Function {
-  constructor(url) {
-    this.url = url;
-    this.quizes = [];
-    this.questionNum = 0;
-    this.num = 1;
-    this.correctAnswerCount = 0;
-  }
-  getQuizApi() {
-    fetch(this.url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('読み込みエラーです。');
-        }
-      })
-      .then((json) => {
-        for (let i = 0; i < json.results.length; i++) {
-          const quiz = new Quiz(json.results[i]);
-          this.quizes.push(quiz);
-        }
-        //開始ボタン非表示
-        startBtn.style.display = 'none';
+const quizes = [];
+const subTitleVal = document.getElementById('sub-title');
+const messageVal = document.getElementById('message');
+const infoWrap = document.getElementById('info');
+const btnWrap = document.getElementById('btn-wrap');
+let questionNum = 0;
+let num = 1;
+let correctAnswerCount = 0;
 
-        //ロード中のメッセージ・タイトルの変更
-        subTitleVal.textContent = '取得中';
-        messageVal.textContent = '少々お待ち下さい・・・';
-        window.setTimeout(this._firstQuiz.bind(this), 2000);
-        
-      });
-  }
-  //最初の問題
-  _firstQuiz() {
-    this.quizes[this.questionNum].showQuiz();
-    this.quizes[this.questionNum].createAnswerBtn();
-    this._createAddEvents();
-    subTitleVal.textContent = `問題${this.num}`;
-  }
-  //答えを押した後
-  _replyQuiz(event) {
-    if (this.num >= 10) {
-      const answerList = document.querySelector('.answer-list');
-      btnWrap.removeChild(answerList);
-      for (let i = infoWrap.childNodes.length; i > 0; i--) {
-        const child = infoWrap.childNodes[i - 1];
-        infoWrap.removeChild(child);
+function getQuizApi() {
+  fetch('https://opentdb.com/api.php?amount=10')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('読み込みエラーです。');
       }
-      subTitleVal.textContent = `あなたの正答数は${this.correctAnswerCount}です。`;
-      messageVal.textContent =
-        '再チャレンジする場合は～開始ボタン～を押してください。';
-      startBtn.style.display = 'block';
-      startBtn.textContent = 'ホームに戻る';
-      startBtn.addEventListener('click', this._restartQuiz);
-    } else {
-      if (
-        event.target.textContent === this.quizes[this.questionNum].correctAnswer
-      ) {
-        this.correctAnswerCount++;
+    })
+    .then((json) => {
+      for (let i = 0; i < json.results.length; i++) {
+        const quiz = new Quiz(json.results[i]);
+        quizes.push(quiz);
       }
-      subTitleVal.textContent = `問題${++this.num}`;
-      for (let i = infoWrap.childNodes.length; i > 0; i--) {
-        const child = infoWrap.childNodes[i - 1];
-        infoWrap.removeChild(child);
-      }
-      const answerList = document.querySelector('.answer-list');
-      btnWrap.removeChild(answerList);
+      //開始ボタン非表示
+      startBtn.style.display = 'none';
 
-      this.quizes[++this.questionNum].showQuiz();
-      this.quizes[this.questionNum].createAnswerBtn();
-      this._createAddEvents();
+      //ロード中のメッセージ・タイトルの変更
+      subTitleVal.textContent = '取得中';
+      messageVal.textContent = '少々お待ち下さい・・・';
+      window.setTimeout(firstQuiz, 2000);
+    });
+}
+//最初の問題
+function firstQuiz() {
+  quizes[questionNum].showQuiz();
+  quizes[questionNum].createAnswerBtn();
+  createAddEvents();
+  subTitleVal.textContent = `問題${num}`;
+}
+//答えを押した後
+function replyQuiz(event) {
+  if (num >= 10) {
+    const answerList = document.querySelector('.answer-list');
+    btnWrap.removeChild(answerList);
+    for (let i = infoWrap.childNodes.length; i > 0; i--) {
+      const child = infoWrap.childNodes[i - 1];
+      infoWrap.removeChild(child);
     }
-  }
-
-  //各ボタンにaddEventの追加
-  _createAddEvents() {
-    const answerBtns = document.getElementsByClassName('answer-btn');
-    for (let i = 0; i < answerBtns.length; i++) {
-      const answerBtn = answerBtns[i];
-      answerBtn.addEventListener('click', this._replyQuiz.bind(this));
+    subTitleVal.textContent = `あなたの正答数は${correctAnswerCount}です。`;
+    messageVal.textContent =
+      '再チャレンジする場合は～開始ボタン～を押してください。';
+    startBtn.style.display = 'block';
+    startBtn.textContent = 'ホームに戻る';
+    startBtn.addEventListener('click', restartQuiz);
+  } else {
+    if (event.target.textContent === quizes[questionNum].correctAnswer) {
+      correctAnswerCount++;
     }
+    subTitleVal.textContent = `問題${++num}`;
+    for (let i = infoWrap.childNodes.length; i > 0; i--) {
+      const child = infoWrap.childNodes[i - 1];
+      infoWrap.removeChild(child);
+    }
+    const answerList = document.querySelector('.answer-list');
+    btnWrap.removeChild(answerList);
+
+    quizes[++questionNum].showQuiz();
+    quizes[questionNum].createAnswerBtn();
+    createAddEvents();
   }
-  //リスタートするボタン
-  _restartQuiz() {
-    window.location.reload();
+}
+
+//各ボタンにaddEventの追加
+function createAddEvents() {
+  const answerBtns = document.getElementsByClassName('answer-btn');
+  for (let i = 0; i < answerBtns.length; i++) {
+    const answerBtn = answerBtns[i];
+    answerBtn.addEventListener('click', replyQuiz);
   }
+}
+//リスタートするボタン
+function restartQuiz() {
+  window.location.reload();
 }
--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 const startBtn = document.getElementById('start-btn');
-const subTitleVal = document.getElementById('sub-title');
-const messageVal = document.getElementById('message');
-const infoWrap = document.getElementById('info');
-const btnWrap = document.getElementById('btn-wrap');
 
-const quizFunction = new Function('https://opentdb.com/api.php?amount=10');
-startBtn.addEventListener('click', quizFunction.getQuizApi.bind(quizFunction));
+startBtn.addEventListener('click', getQuizApi);
 
